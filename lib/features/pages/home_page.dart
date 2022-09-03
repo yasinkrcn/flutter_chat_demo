6f9dcb7f90af380a9ef53c586da3b235e3d6a369
Yasin Karacan <23yasin1999@gmail.com>
2022-09-03 15:42:55 +0300

    message box updated

---
+++ lib/features/pages/home_page.dart
@@ -0,0 +1,510 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+
+import '../../core/constants/color_constants.dart';
+import '../../core/constants/firestore_constants.dart';
+import '../../core/shared_widgets/loading_view.dart';
+import '../../core/utils/debouncer.dart';
+import '../../core/utils/utilities.dart';
+import '../models/_models_exports.dart';
+
+import '../providers/auth_provider.dart';
+import '../providers/home_provider.dart';
+import '_pages_exports.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  HomePageState({Key? key});
+
+  final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final ScrollController listScrollController = ScrollController();
+
+  int _limit = 20;
+  int _limitIncrement = 20;
+  String _textSearch = "";
+  bool isLoading = false;
+
+  late AuthProvider authProvider;
+  late String currentUserId;
+  late HomeProvider homeProvider;
+  Debouncer searchDebouncer = Debouncer(milliseconds: 300);
+  StreamController<bool> btnClearController = StreamController<bool>();
+  TextEditingController searchBarTec = TextEditingController();
+
+  List<PopupChoices> choices = <PopupChoices>[
+    PopupChoices(title: 'Settings', icon: Icons.settings),
+    PopupChoices(title: 'Log out', icon: Icons.exit_to_app),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    authProvider = context.read<AuthProvider>();
+    homeProvider = context.read<HomeProvider>();
+
+    if (authProvider.getUserFirebaseId()?.isNotEmpty == true) {
+      currentUserId = authProvider.getUserFirebaseId()!;
+    } else {
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => LoginPage()),
+        (Route<dynamic> route) => false,
+      );
+    }
+    registerNotification();
+    configLocalNotification();
+    listScrollController.addListener(scrollListener);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    btnClearController.close();
+  }
+
+  void registerNotification() {
+    firebaseMessaging.requestPermission();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('onMessage: $message');
+      if (message.notification != null) {
+        showNotification(message.notification!);
+      }
+      return;
+    });
+
+    firebaseMessaging.getToken().then((token) {
+      print('push token: $token');
+      if (token != null) {
+        homeProvider.updateDataFirestore(FirestoreConstants.pathUserCollection, currentUserId, {'pushToken': token});
+      }
+    }).catchError((err) {
+      Fluttertoast.showToast(msg: err.message.toString());
+    });
+  }
+
+  void configLocalNotification() {
+    AndroidInitializationSettings initializationSettingsAndroid =
+        const AndroidInitializationSettings('@drawable/ic_stat_cloud_download');
+    IOSInitializationSettings initializationSettingsIOS = const IOSInitializationSettings();
+    InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  void scrollListener() {
+    if (listScrollController.offset >= listScrollController.position.maxScrollExtent &&
+        !listScrollController.position.outOfRange) {
+      setState(() {
+        _limit += _limitIncrement;
+      });
+    }
+  }
+
+  void onItemMenuPress(PopupChoices choice) {
+    if (choice.title == 'Log out') {
+      handleSignOut();
+    } else {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingsPage()));
+    }
+  }
+
+  void showNotification(RemoteNotification remoteNotification) async {
+    AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      Platform.isAndroid ? 'com.example.flutter_chat_demo' : 'com.example.flutter_chat_demo',
+      'Flutter chat demo',
+      playSound: true,
+      enableVibration: true,
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    IOSNotificationDetails iOSPlatformChannelSpecifics = const IOSNotificationDetails();
+    NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
+
+    print(remoteNotification);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      remoteNotification.title,
+      remoteNotification.body,
+      platformChannelSpecifics,
+      payload: json.encode(remoteNotification),
+    );
+  }
+
+  Future<bool> onBackPress() {
+    openDialog();
+    return Future.value(false);
+  }
+
+  Future<void> openDialog() async {
+    switch (await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            clipBehavior: Clip.hardEdge,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            contentPadding: EdgeInsets.zero,
+            children: [
+              Container(
+                color: ColorHelper.themeColor,
+                padding: const EdgeInsets.only(bottom: 10, top: 10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10),
+                      child: const Icon(
+                        Icons.exit_to_app,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      'Exit app',
+                      style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const Text(
+                      'Are you sure to exit app?',
+                      style: TextStyle(color: Colors.white70, fontSize: 14),
+                    ),
+                  ],
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, 0);
+                },
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      child: const Icon(
+                        Icons.cancel,
+                        color: ColorHelper.primaryColor,
+                      ),
+                    ),
+                    const Text(
+                      'Cancel',
+                      style: TextStyle(color: ColorHelper.primaryColor, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, 1);
+                },
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      child: const Icon(
+                        Icons.check_circle,
+                        color: ColorHelper.primaryColor,
+                      ),
+                    ),
+                    const Text(
+                      'Yes',
+                      style: TextStyle(color: ColorHelper.primaryColor, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          );
+        })) {
+      case 0:
+        break;
+      case 1:
+        exit(0);
+    }
+  }
+
+  Future<void> handleSignOut() async {
+    authProvider.handleSignOut();
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (context) => LoginPage()),
+      (Route<dynamic> route) => false,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Message',
+          style: TextStyle(color: ColorHelper.white),
+        ),
+        elevation: 0,
+        centerTitle: true,
+        actions: [
+          buildPopupMenu(),
+        ],
+      ),
+      body: WillPopScope(
+        onWillPop: onBackPress,
+        child: Stack(
+          children: [
+            // List
+            Column(
+              children: [
+                buildSearchBar(),
+                Expanded(
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: homeProvider.getStreamFireStore(FirestoreConstants.pathUserCollection, _limit, _textSearch),
+                    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasData) {
+                        if ((snapshot.data?.docs.length ?? 0) > 0) {
+                          return ListView.builder(
+                            padding: const EdgeInsets.all(10),
+                            itemBuilder: (context, index) => buildItem(context, snapshot.data?.docs[index]),
+                            itemCount: snapshot.data?.docs.length,
+                            controller: listScrollController,
+                          );
+                        } else {
+                          return const Center(
+                            child: Text("No users"),
+                          );
+                        }
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            color: ColorHelper.themeColor,
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+
+            // Loading
+            Positioned(
+              child: isLoading ? const LoadingView() : const SizedBox.shrink(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildSearchBar() {
+    return Container(
+      height: 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: ColorHelper.greyColor2,
+      ),
+      padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+      margin: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Icon(Icons.search, color: ColorHelper.greyColor, size: 20),
+          const SizedBox(width: 5),
+          Expanded(
+            child: TextFormField(
+              textInputAction: TextInputAction.search,
+              controller: searchBarTec,
+              onChanged: (value) {
+                searchDebouncer.run(() {
+                  if (value.isNotEmpty) {
+                    btnClearController.add(true);
+                    setState(() {
+                      _textSearch = value;
+                    });
+                  } else {
+                    btnClearController.add(false);
+                    setState(() {
+                      _textSearch = "";
+                    });
+                  }
+                });
+              },
+              decoration: const InputDecoration.collapsed(
+                hintText: 'Search name',
+                hintStyle: TextStyle(fontSize: 13, color: ColorHelper.greyColor),
+              ),
+              style: const TextStyle(fontSize: 13),
+            ),
+          ),
+          StreamBuilder<bool>(
+              stream: btnClearController.stream,
+              builder: (context, snapshot) {
+                return snapshot.data == true
+                    ? GestureDetector(
+                        onTap: () {
+                          searchBarTec.clear();
+                          btnClearController.add(false);
+                          setState(() {
+                            _textSearch = "";
+                          });
+                        },
+                        child: const Icon(Icons.clear_rounded, color: ColorHelper.greyColor, size: 20))
+                    : const SizedBox.shrink();
+              }),
+        ],
+      ),
+    );
+  }
+
+  Widget buildPopupMenu() {
+    return PopupMenuButton<PopupChoices>(
+      onSelected: onItemMenuPress,
+      itemBuilder: (BuildContext context) {
+        return choices.map((PopupChoices choice) {
+          return PopupMenuItem<PopupChoices>(
+              value: choice,
+              child: Row(
+                children: [
+                  Icon(
+                    choice.icon,
+                    color: ColorHelper.secondaryColor,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    choice.title,
+                    style: const TextStyle(color: ColorHelper.secondaryColor),
+                  ),
+                ],
+              ));
+        }).toList();
+      },
+    );
+  }
+
+  Widget buildItem(BuildContext context, DocumentSnapshot? document) {
+    if (document != null) {
+      UserChat userChat = UserChat.fromDocument(document);
+      if (userChat.id == currentUserId) {
+        return const SizedBox.shrink();
+      } else {
+        return GestureDetector(
+          onTap: () {
+            if (Utilities.isKeyboardShowing()) {
+              Utilities.closeKeyboard(context);
+            }
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatPage(
+                  arguments: ChatPageArguments(
+                    peerId: userChat.id,
+                    peerAvatar: userChat.photoUrl,
+                    peerNickname: userChat.nickname,
+                  ),
+                ),
+              ),
+            );
+          },
+          child: Container(
+            height: 60,
+            decoration: const BoxDecoration(
+              color: Colors.yellow,
+              border: Border(
+                bottom: BorderSide(
+                  color: ColorHelper.greyDarkColor,
+                  width: 0.3,
+                ),
+              ),
+            ),
+            margin: const EdgeInsets.only(bottom: 10, left: 5, right: 5),
+            child: Row(
+              children: [
+                Material(
+                  borderRadius: const BorderRadius.all(Radius.circular(25)),
+                  clipBehavior: Clip.hardEdge,
+                  child: userChat.photoUrl.isNotEmpty
+                      ? Image.network(
+                          userChat.photoUrl,
+                          fit: BoxFit.cover,
+                          width: 50,
+                          height: 50,
+                          loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                            if (loadingProgress == null) return child;
+                            return SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Center(
+                                child: CircularProgressIndicator(
+                                  color: ColorHelper.themeColor,
+                                  value: loadingProgress.expectedTotalBytes != null
+                                      ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                                      : null,
+                                ),
+                              ),
+                            );
+                          },
+                          errorBuilder: (context, object, stackTrace) {
+                            return const Icon(
+                              Icons.account_circle,
+                              size: 50,
+                              color: ColorHelper.greyColor,
+                            );
+                          },
+                        )
+                      : const Icon(
+                          Icons.account_circle,
+                          size: 50,
+                          color: ColorHelper.greyColor,
+                        ),
+                ),
+                Flexible(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          margin: const EdgeInsets.fromLTRB(10, 0, 0, 5),
+                          child: Text(
+                            userChat.nickname,
+                            maxLines: 1,
+                            style: const TextStyle(color: ColorHelper.secondaryColor),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          margin: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                          child: Text(
+                            'About me: ${userChat.aboutMe}',
+                            maxLines: 1,
+                            style: const TextStyle(color: ColorHelper.secondaryColor),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+}
