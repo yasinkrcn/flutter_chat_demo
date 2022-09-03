5afda4c635465424139923005a4f6dd700be8730
Yasin Karacan <23yasin1999@gmail.com>
2022-09-03 15:41:29 +0300

    ColorHelper moved

---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class ColorHelper {
+  static const Color themeColor = Color(0xfff5a623);
+  static const Color primaryColor = Color(0xFF0081FE);
+  static const Color greyColor = Color(0xffaeaeae);
+  static const Color greyColor2 = Color(0xffE8E8E8);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color secondaryColor = Color(0xFF0A142F);
+  static const Color greyDarkColor = Color(0xFF687684);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color successColor = Color(0xFF55B938);
+  static const Color errorColor = Color(0xFFFA1313);
+  static const Color lightBlueColor = Color(0xFFCCE6FF);
+  static const Color backgroundColor = Color(0xFFF9F9FF);
+}
