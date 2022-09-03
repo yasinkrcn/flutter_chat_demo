c0e78abb49e6b26d032c4876165e26928c31833c
Yasin Karacan <23yasin1999@gmail.com>
2022-09-03 15:41:36 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_demo/constants/app_constants.dart';
+import 'package:flutter_chat_demo/core/constants/app_constants.dart';
+import 'package:flutter_chat_demo/core/constants/color_constants.dart';
+import 'package:flutter_chat_demo/features/providers/_providers_exports.dart';
 import 'package:flutter_chat_demo/firebase_options.dart';
-import 'package:flutter_chat_demo/providers/auth_provider.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'constants/color_constants.dart';
-import 'pages/_exports_pages.dart';
-import 'providers/providers.dart';
+import 'features/pages/splash_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -67,9 +66,12 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: AppConstants.appTitle,
         theme: ThemeData(
-          primaryColor: ColorHelper.themeColor,
+          primaryColor: ColorHelper.blackColor,
+          scaffoldBackgroundColor: ColorHelper.backgroundColor,
+          backgroundColor: ColorHelper.backgroundColor,
+          primarySwatch: Colors.blue,
         ),
-        home: SplashPage(),
+        home: const SplashPage(),
         debugShowCheckedModeBanner: false,
       ),
     );
