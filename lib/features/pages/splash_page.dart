b2de9666aee89082577fc771f0ec5525cc04dfc5
Yasin Karacan <23yasin1999@gmail.com>
2022-09-03 15:42:22 +0300

    moved

---
+++ lib/features/pages/splash_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_demo/features/providers/_providers_exports.dart';
+
+import 'package:provider/provider.dart';
+
+import '../../core/constants/color_constants.dart';
+import '_pages_exports.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  SplashPageState createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 1), () {
+      // just delay for showing this slash page clearer because it too fast
+      checkSignedIn();
+    });
+  }
+
+  void checkSignedIn() async {
+    AuthProvider authProvider = context.read<AuthProvider>();
+    bool isLoggedIn = await authProvider.isLoggedIn();
+    if (isLoggedIn) {
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+      return;
+    }
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              "images/app_icon.png",
+              width: 100,
+              height: 100,
+            ),
+            const SizedBox(height: 20),
+            const SizedBox(
+              width: 20,
+              height: 20,
+              child: CircularProgressIndicator(color: ColorHelper.themeColor),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
