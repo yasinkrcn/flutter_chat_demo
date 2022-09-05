1dee8ba553a1cfd4b85f4bff78eff86d55e439c5
Yasin Karacan <23yasin1999@gmail.com>
2022-09-05 17:10:36 +0300

    comment TODO

--- lib/core/utils/route/router.dart
+++ lib/core/utils/route/router.dart
@@ -1,31 +1,31 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_chat_demo/features/pages/_pages_exports.dart';
+// import 'package:flutter/cupertino.dart';
 
-class PageRoutes {
-  static const String splashPage = "/splashPage";
-  static const String loginPage = "/loginPage";
-  static const String homePage = "/homePage";
-  static const String settingsPage = "/settingsPage";
-  static const String chatPage = "/chatPage";
-  static const String fullPhotoPage = "/fullPhotoPage";
-}
 
-Route<dynamic> generateRoute(RouteSettings settings) {
-  switch (settings.name) {
-    case PageRoutes.splashPage:
-      return CupertinoPageRoute(builder: (_) => const SplashPage());
-    case PageRoutes.loginPage:
-      return CupertinoPageRoute(builder: (_) => const LoginPage());
-    case PageRoutes.homePage:
-      return CupertinoPageRoute(builder: (_) => const HomePage());
-    case PageRoutes.settingsPage:
-      return CupertinoPageRoute(builder: (_) => const SettingsPage());
-    case PageRoutes.chatPage:
-      return CupertinoPageRoute(builder: (_) => ChatPage());
-    case PageRoutes.fullPhotoPage:
-      return CupertinoPageRoute(builder: (_) => FullPhotoPage());
+// class PageRoutes {
+//   static const String splashPage = "/splashPage";
+//   static const String loginPage = "/loginPage";
+//   static const String homePage = "/homePage";
+//   static const String settingsPage = "/settingsPage";
+//   static const String chatPage = "/chatPage";
+//   static const String fullPhotoPage = "/fullPhotoPage";
+// }
 
-    default:
-      return CupertinoPageRoute(builder: (_) => const LoginPage());
-  }
-}
+// Route<dynamic> generateRoute(RouteSettings settings) {
+//   switch (settings.name) {
+//     case PageRoutes.splashPage:
+//       return CupertinoPageRoute(builder: (_) => const SplashPage());
+//     case PageRoutes.loginPage:
+//       return CupertinoPageRoute(builder: (_) => const LoginPage());
+//     case PageRoutes.homePage:
+//       return CupertinoPageRoute(builder: (_) => const HomePage());
+//     case PageRoutes.settingsPage:
+//       return CupertinoPageRoute(builder: (_) => const SettingsPage());
+//     case PageRoutes.chatPage:
+//       return CupertinoPageRoute(builder: (_) => ChatPage());
+//     case PageRoutes.fullPhotoPage:
+//       return CupertinoPageRoute(builder: (_) => FullPhotoPage());
+
+//     default:
+//       return CupertinoPageRoute(builder: (_) => const LoginPage());
+//   }
+// }
